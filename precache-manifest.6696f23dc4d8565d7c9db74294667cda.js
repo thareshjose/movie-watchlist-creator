fdda73314d79d1a25548d35f2d7595691080fc6c
Tharesh <99tharesh@gmail.com>
2019-08-04 17:16:41 +0530

    Updates

---
+++ precache-manifest.6696f23dc4d8565d7c9db74294667cda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fba231f31051c8861f79091d69527cdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce2a7cec63000cc2f937",
+    "url": "/static/css/main.f6ebc95d.chunk.css"
+  },
+  {
+    "revision": "bb7745aee26e3e51623a",
+    "url": "/static/js/2.65880d7e.chunk.js"
+  },
+  {
+    "revision": "ce2a7cec63000cc2f937",
+    "url": "/static/js/main.d4805fa6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d0264c08b53d8ef671ceef66c6caa6c",
+    "url": "/static/media/trending-icon.5d0264c0.png"
+  }
+]);
--- src/Components/LikeMovie.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import saveIcon from "../images/save.png";
-
-export default function LikeMovie(props) {
-  return (
-    <img src={saveIcon} alt="save" onClick={() => props.likeMovie("add")} />
-  );
-}
